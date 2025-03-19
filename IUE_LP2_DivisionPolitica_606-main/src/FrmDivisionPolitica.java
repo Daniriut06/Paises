830bb5058bc97dcf5a6f9f251e53f163c2d4c183
Daniriut06 <daniel.luna.mono@gmail.com>
2025-03-19 11:20:26 -0500

    ola

---
+++ IUE_LP2_DivisionPolitica_606-main/src/FrmDivisionPolitica.java
@@ -0,0 +1,108 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
+import javax.swing.JTree;
+import javax.swing.WindowConstants;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import entidades.Pais;
+
+public class FrmDivisionPolitica extends JFrame {
+
+    private JTree arbol;
+    DefaultMutableTreeNode nodoRaiz;
+    JLabel lblMapa;
+
+    public FrmDivisionPolitica() {
+        setSize(600, 400);
+        setTitle("División Política");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JToolBar tbDivisionPolitica = new JToolBar();
+
+        JButton btnHimno = new JButton();
+        btnHimno.setIcon(new ImageIcon(getClass().getResource("/iconos/himno.jpg")));
+        btnHimno.setToolTipText("Reproducir Himno");
+        btnHimno.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                reproducirHimno();
+            }
+        });
+        tbDivisionPolitica.add(btnHimno);
+
+        // Crear el nodo raíz del árbol
+        nodoRaiz = new DefaultMutableTreeNode("Paises");
+
+        // Crear el modelo del árbol
+        arbol = new JTree(new DefaultTreeModel(nodoRaiz));
+        JScrollPane spArbol = new JScrollPane(arbol);
+
+        // Agregar listener para detectar selección de nodos
+        arbol.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                mostrarMapa();
+            }
+        });
+
+        // Crear
+        lblMapa = new JLabel();
+        JScrollPane spMapa = new JScrollPane(lblMapa);
+
+        // Divisor entre el árbol y el mapa
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, spArbol, spMapa);
+        splitPane.setDividerLocation(250); // Tamaño inicial del árbol
+
+        getContentPane().add(tbDivisionPolitica, BorderLayout.NORTH);
+        getContentPane().add(splitPane, BorderLayout.CENTER);
+
+        cargarDatos();
+    }
+
+    private List<Pais> paises;
+    
+    private void cargarDatos() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String nombreArchivo = System.getProperty("user.dir") + "/src/datos/DivisionPolitica.json";
+
+        //obtener los datos desde el archivo yeison
+        try{
+            paises = objectMapper.readValue(new File(nombreArchivo), objectMapper.getTypeFactory().constructCollectionType(List.class, Pais.class));
+            if (paises != null){
+                for (Pais pais : paises){
+                    DefaultMutableTreeNode nodoPais = new DefaultMutableTreeNode(pais.getNombre());
+                    nodoRaiz.add(nodoPais);
+                }
+
+            }
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "no se pudieron cargar los datos" + ex);
+
+        }
+
+    }
+    private void mostrarMapa() {
+
+    }
+
+    private void reproducirHimno() {
+
+    }
+
+}
